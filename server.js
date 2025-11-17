6c15d4e536afdb0f71eddf1c858769e5f5e148ab
Alisson <alisson.oliveiraferreira23@gmail.com>
2025-11-17 20:36:47 -0300

    ajustes oara o docker

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const port = 3003;
 
-app.listen(port, () => {
-  console.log(`Servidor do quarto rodando em http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor do quarto rodando em http://0.0.0.0:${port}`);
 });
